laminas/laminas-httphandlerrunner

commit f41b72df527ad5cd9e1814fa62efc52271473f9c

bugfix: ensure proper function execution in CI
Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

src/Emitter/SapiEmitterTrait.php
@@ -8,6 +8,7 @@ use Laminas\HttpHandlerRunner\Exception\EmitterException;
 use Psr\Http\Message\ResponseInterface;
 
 use function assert;
+use function function_exists;
 use function header;
 use function headers_sent;
 use function is_string;
@@ -29,8 +30,7 @@ trait SapiEmitterTrait
      */
     private function assertNoPreviousOutput(): void
     {
-        /** @psalm-suppress DocblockTypeContradiction {@see \Laminas\HttpHandlerRunner\Emitter\headers_sent()} */
-        if (headers_sent()) {
+        if ($this->headersSent()) {
             throw EmitterException::forHeadersSent();
         }
 
@@ -56,7 +56,7 @@ trait SapiEmitterTrait
         $reasonPhrase = $response->getReasonPhrase();
         $statusCode   = $response->getStatusCode();
 
-        header(sprintf(
+        $this->header(sprintf(
             'HTTP/%s %d%s',
             $response->getProtocolVersion(),
             $statusCode,
@@ -81,7 +81,7 @@ trait SapiEmitterTrait
             $name  = $this->filterHeader($header);
             $first = $name !== 'Set-Cookie';
             foreach ($values as $value) {
-                header(sprintf(
+                $this->header(sprintf(
                     '%s: %s',
                     $name,
                     $value
@@ -97,5 +97,26 @@ trait SapiEmitterTrait
     private function filterHeader(string $header): string
     {
         return ucwords($header, '-');
+    }
+
+    private function headersSent(): bool
+    {
+        if (function_exists('Laminas\HttpHandlerRunner\Emitter\headers_sent')) {
+            // phpcs:ignore SlevomatCodingStandard.Namespaces.ReferenceUsedNamesOnly.ReferenceViaFullyQualifiedName
+            return \Laminas\HttpHandlerRunner\Emitter\headers_sent();
+        }
+
+        return headers_sent();
+    }
+
+    private function header(string $headerName, bool $replace, int $statusCode): void
+    {
+        if (function_exists('Laminas\HttpHandlerRunner\Emitter\headers_sent')) {
+            // phpcs:ignore SlevomatCodingStandard.Namespaces.ReferenceUsedNamesOnly.ReferenceViaFullyQualifiedName
+            \Laminas\HttpHandlerRunner\Emitter\header($headerName, $replace, $statusCode);
+            return;
+        }
+
+        header($headerName, $replace, $statusCode);
     }
 }

test/TestAsset/SapiResponse.php
@@ -9,8 +9,6 @@ use LaminasTest\HttpHandlerRunner\TestAsset\HeaderStack;
 
 /**
  * Have headers been sent?
- *
- * @return false
  */
 function headers_sent(): bool
 {
@@ -20,11 +18,11 @@ function headers_sent(): bool
 /**
  * Emit a header, without creating actual output artifacts
  */
-function header(string $header, bool $replace = true, ?int $httpResponseCode = null): void
+function header(string $headerName, bool $replace = true, ?int $httpResponseCode = null): void
 {
     HeaderStack::push(
         [
-            'header'      => $header,
+            'header'      => $headerName,
             'replace'     => $replace,
             'status_code' => $httpResponseCode,
         ]

commit 3c663dcdb58ceaa390cbf7d587fa4a7b4b50cbf3

Fix function_exists() check in SapiEmitterTrait
This is an *obvious fix* for a copy and paste mistake.

Bug introduced in f41b72df527ad5cd9e1814fa62efc52271473f9c.

src/Emitter/SapiEmitterTrait.php
@@ -115,7 +115,7 @@ trait SapiEmitterTrait
 
     private function header(string $headerName, bool $replace, int $statusCode): void
     {
-        if (function_exists('Laminas\HttpHandlerRunner\Emitter\headers_sent')) {
+        if (function_exists('Laminas\HttpHandlerRunner\Emitter\header')) {
             // phpcs:ignore SlevomatCodingStandard.Namespaces.ReferenceUsedNamesOnly.ReferenceViaFullyQualifiedName
             \Laminas\HttpHandlerRunner\Emitter\header($headerName, $replace, $statusCode);
             return;